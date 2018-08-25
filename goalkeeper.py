b424b68d17794ddf9982d01ffdfebf0f6ab3a64d
petro.sernivka <p.sernivka@gmail.com>
2018-08-25 12:51:25 +0300

    add goalkeeper.py

---
+++ goalkeeper.py
@@ -0,0 +1,59 @@
+import pygame
+from random import shuffle
+
+pygame.init()
+disp_width = 1200
+disp_height = 600
+win = pygame.display.set_mode((disp_width, disp_height))
+
+pygame.display.set_caption('Goalkeeper Game')
+
+bg = pygame.image.load('bg.png')
+keep_positions = [pygame.image.load('keep_1.png'), pygame.image.load('keep_2.png'),
+            pygame.image.load('keep_3.png'), pygame.image.load('keep_4.png'),
+            pygame.image.load('keep_5.png'), pygame.image.load('keep_6.png')]
+ball = pygame.image.load('ball.png')
+
+width_keep = 70
+height_keep = 126
+width_goal = 800
+height_goal = 240
+x_pos_keep = (disp_width - width_keep) // 2
+y_pos_keep = (disp_height - height_goal) // 2 + height_goal - height_keep
+x_pos_goal = (disp_width - width_goal) // 2
+y_pos_goal = (disp_height - height_goal) // 2
+speed_keep_width = (width_goal - width_keep) // 2
+speed_keep_height = height_goal - height_keep
+
+left = False
+right = False
+up = False
+down = False
+
+cycle_time = 1000
+shots = [(20, 0, 1, 1), (378, 0, 1, 1), (1155, 0, -1, 1), (20, 590, 1, -1), (378, 590, 1, -1), (1155, 590, -1, -1)]
+keep_position = 4
+
+run = True
+while run:
+    pygame.time.delay(cycle_time)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    run_ball = True
+    i = 0
+    shuffle(shots)
+    while run_ball:
+        pygame.time.delay(cycle_time // 20)
+        win.blit(bg, (0, 0))
+
+        win.blit(keep_positions[keep_position], ((disp_width - 70) // 2, (disp_height - height_goal) // 2 + height_goal - 126))
+        win.blit(ball, (shots[0][0] + 10.5 * i * shots[0][2], shots[0][1] + 10.5 * i * shots[0][3]))
+        pygame.display.update()
+        i += 1
+        if i > 20:
+            run_ball = False
+
+pygame.quit()
