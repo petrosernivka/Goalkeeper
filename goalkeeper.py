9e0bba6092cbbb4af0b4f33c21fa7fcbc096a01f
petro.sernivka <p.sernivka@gmail.com>
2018-08-26 21:56:43 +0300

    when the ball and keeper are in the same position - a green star appears, else - a red star

--- goalkeeper.py
+++ goalkeeper.py
@@ -20,7 +20,8 @@ keep_positions = [(pygame.image.load('keep_1.png'), 223, 202),
                 (pygame.image.load('keep_6.png'), 802, 343)]
 
 cycle_time = 150
-shots = [(20, 0, 1, 1), (378, 0, 1, 1), (1155, 0, -1, 1), (20, 590, 1, -1), (378, 560, 1, -1), (1155, 590, -1, -1)]
+shots = [(20, 0, 1, 1, 0), (378, 0, 1, 1, 1), (1155, 0, -1, 1, 2),
+        (20, 590, 1, -1, 3), (378, 560, 1, -1, 4), (1155, 590, -1, -1, 5)]
 keep_position = 4
 ball_run_index = 0
 
@@ -57,8 +58,11 @@ while run:
     win.blit(bg, (0, 0))
     win.blit(keep_positions[keep_position][0], (keep_positions[keep_position][1], keep_positions[keep_position][2]))
 
-    # if ball_run_index > 210: #and keeper is in same position as ball_run_index
-    #     win.blit(ball, (shots[0][0] + min(210, ball_run_index) * shots[0][2], shots[0][1] + min(210, ball_run_index) * shots[0][3]))
+    if ball_run_index > 210:
+        if shots[0][4] == keep_position:
+            win.blit(star_green, (shots[0][0] + 210 * shots[0][2] - 5, shots[0][1] + 210 * shots[0][3] - 5))
+        else:
+            win.blit(star_red, (shots[0][0] + 210 * shots[0][2] - 5, shots[0][1] + 210 * shots[0][3] - 5))
 
     win.blit(ball, (shots[0][0] + min(210, ball_run_index) * shots[0][2], shots[0][1] + min(210, ball_run_index) * shots[0][3]))
     pygame.display.update()
